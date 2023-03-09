685325466ea624a2a700b1174f4bf2b45839a24e
Justin Carl <justincarljuanico011@gmail.com>
2023-03-09 16:49:08 +0800

    Add Migration Table

---
+++ database/migrations/2023_03_09_082529_create_schedules_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('vessel_id')->unsigned();
+            $table->string('origin');
+            $table->string('destination');
+            $table->date('departure_date');
+            $table->date('departure_time');
+            $table->foreign('vessel_id')->references('id')->on('vessels')->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
