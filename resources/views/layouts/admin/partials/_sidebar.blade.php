ccb585b95f54485cdc4c9bf35c90dc03e4d38765
Franz Gee <franz@Franz.local>
2023-03-17 17:42:11 +0800

    Added the User's name back to the sidebar

--- resources/views/layouts/admin/partials/_sidebar.blade.php
+++ resources/views/layouts/admin/partials/_sidebar.blade.php
@@ -14,7 +14,7 @@
           <!-- <img src="" class="img-circle elevation-2" alt="User Image"> -->
         </div>
         <div class="info">
-          
+          <a href="#" class="d-block">{{ auth()->user()->name }}</a>
         </div>
       </div>
 
