26b4f31cd077e911617bf4147510897065053e88
Franz Gee <franz@Franz.local>
2023-03-23 06:34:45 +0800

    Forgot Password

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,60 +1,61 @@
-@extends('layouts.app')
+@extends('layouts.front.auth')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
+<div class="container my-5">
+    <div class="row justify-content-center my-5">
+        <div class="col-md-6 my-5">
+            <div class="text-center">
+              <img src="{{ asset('assets/logo/rec/clr-25.png') }}" alt="" srcset="">
+            </div>
+            <div class="card my-5">
                 <div class="card-header">{{ __('Reset Password') }}</div>
-
                 <div class="card-body">
                     <form method="POST" action="{{ route('password.update') }}">
                         @csrf
-
                         <input type="hidden" name="token" value="{{ $token }}">
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                        <div class="input-group mb-3">
+                          <!-- <input type="email" class="form-control" placeholder="Email"> -->
+                          <input id="email" type="email" placeholder="Your email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                          <div class="input-group-append">
+                            <div class="input-group-text">
+                              <span class="fas fa-envelope"></span>
                             </div>
+                          </div>
+                          @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                          @enderror
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                        <div class="input-group mb-3">
+                          <!-- <input type="password" class="form-control" placeholder="Password"> -->
+                          <input id="password" type="password" placeholder="Your password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                          <div class="input-group-append">
+                            <div class="input-group-text">
+                              <span class="fas fa-lock"></span>
                             </div>
+                          </div>
+                          @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                          @enderror
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                        <div class="input-group mb-3">
+                          <!-- <input type="password" class="form-control" placeholder="Retype password"> -->
+                          <input id="password-confirm" placeholder="Confirm password" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                          <div class="input-group-append">
+                            <div class="input-group-text">
+                              <span class="fas fa-lock"></span>
                             </div>
+                          </div>
+                          
                         </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
+                        <div class="row">
+                          <div class="col-6">
+                            <button type="submit" class="btn btn-primary btn-block">Reset</button>
+                          </div>
                         </div>
                     </form>
                 </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,77 +12,73 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
-
-                        <form method="POST" action="{{ route('register') }}">
-                            @csrf
-                            <div class="input-group mb-3">
-                              <!-- <input type="text" class="form-control" placeholder="Full name"> -->
-                              <input id="name" type="text" placeholder="Your name" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-                              <div class="input-group-append">
-                                <div class="input-group-text">
-                                  <span class="fas fa-user"></span>
-                                </div>
-                              </div>
-                              @error('name')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                              @enderror
+                        <div class="input-group mb-3">
+                          <!-- <input type="text" class="form-control" placeholder="Full name"> -->
+                          <input id="name" type="text" placeholder="Your name" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                          <div class="input-group-append">
+                            <div class="input-group-text">
+                              <span class="fas fa-user"></span>
                             </div>
-                            <div class="input-group mb-3">
-                              <!-- <input type="email" class="form-control" placeholder="Email"> -->
-                              <input id="email" type="email" placeholder="Your email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-                              <div class="input-group-append">
-                                <div class="input-group-text">
-                                  <span class="fas fa-envelope"></span>
-                                </div>
-                              </div>
-                              @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                              @enderror
+                          </div>
+                          @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                          @enderror
+                        </div>
+                        <div class="input-group mb-3">
+                          <!-- <input type="email" class="form-control" placeholder="Email"> -->
+                          <input id="email" type="email" placeholder="Your email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                          <div class="input-group-append">
+                            <div class="input-group-text">
+                              <span class="fas fa-envelope"></span>
                             </div>
-                            <div class="input-group mb-3">
-                              <!-- <input type="password" class="form-control" placeholder="Password"> -->
-                              <input id="password" type="password" placeholder="Your password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-                              <div class="input-group-append">
-                                <div class="input-group-text">
-                                  <span class="fas fa-lock"></span>
-                                </div>
-                              </div>
-                              @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                              @enderror
+                          </div>
+                          @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                          @enderror
+                        </div>
+                        <div class="input-group mb-3">
+                          <!-- <input type="password" class="form-control" placeholder="Password"> -->
+                          <input id="password" type="password" placeholder="Your password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                          <div class="input-group-append">
+                            <div class="input-group-text">
+                              <span class="fas fa-lock"></span>
                             </div>
-                            <div class="input-group mb-3">
-                              <!-- <input type="password" class="form-control" placeholder="Retype password"> -->
-                              <input id="password-confirm" placeholder="Confirm password" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                              <div class="input-group-append">
-                                <div class="input-group-text">
-                                  <span class="fas fa-lock"></span>
-                                </div>
-                              </div>
-                              
+                          </div>
+                          @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                          @enderror
+                        </div>
+                        <div class="input-group mb-3">
+                          <!-- <input type="password" class="form-control" placeholder="Retype password"> -->
+                          <input id="password-confirm" placeholder="Confirm password" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                          <div class="input-group-append">
+                            <div class="input-group-text">
+                              <span class="fas fa-lock"></span>
                             </div>
-                            <div class="row">
-                              <div class="col-8">
-                                <div class="icheck-primary">
-                                  <input type="checkbox" id="agreeTerms" name="terms" value="agree">
-                                  <label for="agreeTerms">
-                                   I agree to the <a href="#">terms</a>
-                                  </label>
-                                </div>
-                              </div>
-                              <!-- /.col -->
-                              <div class="col-4">
-                                <button type="submit" class="btn btn-primary btn-block">Register</button>
-                              </div>
-                              <!-- /.col -->
+                          </div>
+                          
+                        </div>
+                        <div class="row">
+                          <div class="col-8">
+                            <div class="icheck-primary">
+                              <input type="checkbox" id="agreeTerms" name="terms" value="agree">
+                              <label for="agreeTerms">
+                                I agree to the <a href="#">terms</a>
+                              </label>
                             </div>
-                        </form>
+                          </div>
+                          <!-- /.col -->
+                          <div class="col-4">
+                            <button type="submit" class="btn btn-primary btn-block">Register</button>
+                          </div>
+                          <!-- /.col -->
+                        </div>
                     </form>
                 </div>
             </div>
